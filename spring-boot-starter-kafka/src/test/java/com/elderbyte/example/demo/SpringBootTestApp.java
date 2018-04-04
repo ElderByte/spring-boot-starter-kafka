59da6a81bb308d636df1f65d264e74e2394c8b59
IsNull <p.buettiker@elderbyte.com>
2018-04-04 14:34:33 +0200

    fixed unit test

---
+++ spring-boot-starter-kafka/src/test/java/com/elderbyte/example/demo/SpringBootTestApp.java
@@ -0,0 +1,25 @@
+package com.elderbyte.example.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@SpringBootApplication
+public class SpringBootTestApp {
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper();
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(SpringBootTestApp.class);
+        Environment env = app.run(args).getEnvironment();
+    }
+
+}
