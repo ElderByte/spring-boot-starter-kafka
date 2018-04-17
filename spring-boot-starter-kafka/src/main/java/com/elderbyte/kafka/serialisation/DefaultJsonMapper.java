fe539b215e625b211f720665a0db90a6300de042
IsNull <p.buettiker@elderbyte.com>
2018-04-17 17:42:06 +0200

    adjusted configuration

--- spring-boot-starter-kafka/src/main/java/com/elderbyte/kafka/serialisation/DefaultJsonMapper.java
+++ spring-boot-starter-kafka/src/main/java/com/elderbyte/kafka/serialisation/DefaultJsonMapper.java
@@ -6,7 +6,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 class DefaultJsonMapper {
     static ObjectMapper buildDefaultMapper(){
         ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, true);
         return mapper;
     }
 }
