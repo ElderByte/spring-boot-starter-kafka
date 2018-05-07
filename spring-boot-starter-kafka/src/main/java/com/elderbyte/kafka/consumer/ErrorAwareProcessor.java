3cf6b025baebf8c6194ee5deafd561437556f230
IsNull <p.buettiker@elderbyte.com>
2018-05-07 15:29:22 +0200

    improved processor helper tools

--- spring-boot-starter-kafka/src/main/java/com/elderbyte/kafka/consumer/ErrorAwareProcessor.java
+++ spring-boot-starter-kafka/src/main/java/com/elderbyte/kafka/consumer/ErrorAwareProcessor.java
@@ -10,14 +10,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.support.Acknowledgment;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
-import java.util.concurrent.Callable;
+
 
 import static java.util.stream.Collectors.toList;
 
-public class ErrorAwareProcessor {
+public class ErrorAwareProcessor<K, V> {
 
 
     /***************************************************************************
@@ -28,9 +28,24 @@ public class ErrorAwareProcessor {
 
     private final Logger log = LoggerFactory.getLogger(ErrorAwareProcessor.class);
     private final MetricsReporter reporter;
+    private final Class<V> valueClazz;
+    private final boolean skipOnError;
+    private final boolean skipOnDtoMappingError;
 
-    public ErrorAwareProcessor(MetricsReporter reporter){
+    /***************************************************************************
+     *                                                                         *
+     * Constructor                                                             *
+     *                                                                         *
+     **************************************************************************/
+
+
+    public ErrorAwareProcessor(Class<V> valueClazz, boolean skipOnError, boolean skipOnDtoMappingError, MetricsReporter reporter){
+        if(valueClazz == null) throw new IllegalArgumentException("valueClazz must not be null");
+        if(reporter == null) throw new IllegalArgumentException("reporter must not be null");
         this.reporter = reporter;
+        this.valueClazz = valueClazz;
+        this.skipOnError = skipOnError;
+        this.skipOnDtoMappingError = skipOnDtoMappingError;
     }
 
 
@@ -40,25 +55,40 @@ public class ErrorAwareProcessor {
      *                                                                         *
      **************************************************************************/
 
+    public boolean convertAndProcess(
+            ConsumerRecord<K, Json> rawRecord,
+            Processor<ConsumerRecord<K, V>> processor,
+            Acknowledgment ack){
 
+        return convertAndProcessAll(
+                Collections.singletonList(rawRecord),
+                records -> processor.proccess(records.get(0)),
+                ack
+                );
+    }
 
-    public <K, V> boolean convertAndProcessAll(
+    public boolean convertAndProcessAll(
             Collection<ConsumerRecord<K, Json>> rawRecords,
-            Class<V> clazz,
             Processor<List<ConsumerRecord<K, V>>> processor,
-            Acknowledgment ack,
-            boolean skipOnError){
+            Acknowledgment ack){
+
+        long start = System.nanoTime();
 
         List<ConsumerRecord<K, V>> records;
 
         try {
             records = rawRecords.stream()
-                    .map(r -> convertRecord(r, clazz))
+                    .map(this::convertRecord)
                     .filter(Objects::nonNull) // Skipped records will be null
                     .collect(toList());
         }catch (Exception e){
-            // todo report failed record
-            throw new UnrecoverableProcessingException("Failed to parse/convert record", e);
+
+            if(skipOnError){
+                return false; // We cant even try to process, just skip this
+            }else{
+                reporter.reportUnrecoverableCrash(rawRecords, e);
+                throw new UnrecoverableProcessingException("Failed to parse/convert record", e);
+            }
         }
 
         // If we are here we have converted the records. Now run the user processing code.
@@ -70,61 +100,10 @@ public class ErrorAwareProcessor {
             processAllErrorLoop(records, processor, ack);
             success = true;
         }
+
+        reporter.reportStreamingMetrics(records.size(), (System.nanoTime() - start) /  (1000 * 1000));
+
         return success;
-    }
-
-    public <K, V> boolean processAllSkipOnError(
-            List<ConsumerRecord<K, V>> records,
-            Processor<List<ConsumerRecord<K, V>>> processor,
-            Acknowledgment ack){
-
-        boolean success;
-        try {
-            processor.proccess(records);
-            success = true;
-        }catch (Exception e){
-            // TODO Report
-            success = false;
-        }finally {
-            if(ack != null) { ack.acknowledge(); }
-        }
-        return success;
-    }
-
-
-    public <K, V> void processAllErrorLoop(
-            List<ConsumerRecord<K, V>> records,
-            Processor<List<ConsumerRecord<K, V>>> processor,
-            Acknowledgment ack){
-
-        if(records == null) throw new IllegalArgumentException("records must not be null");
-        if(processor == null) throw new IllegalArgumentException("processor must not be null");
-        if(ack == null) throw new IllegalArgumentException("ack must not be null");
-
-        int errorCount = 0;
-        boolean errorLoop;
-
-        do{
-            try{
-                processor.proccess(records);
-                ack.acknowledge();
-                errorLoop = false;
-            }catch (Exception e){
-                errorLoop = true;
-                ++errorCount;
-                log.warn("Error while processing records! Retry " + errorCount, e);
-
-                // TODO Report error
-
-                try {
-                    // Error Backoff
-                    Thread.sleep(Math.min(errorCount * 1000, 1000*60));
-
-                    // TODO Report Stuck in loop
-                } catch (InterruptedException e1) { }
-            }
-
-        } while (errorLoop);
     }
 
 
@@ -134,18 +113,76 @@ public class ErrorAwareProcessor {
      *                                                                         *
      **************************************************************************/
 
-    private <K,V> ConsumerRecord<K, V> convertRecord(ConsumerRecord<K, Json> record, Class<V> clazz){
+    private boolean processAllSkipOnError(
+            List<ConsumerRecord<K, V>> records,
+            Processor<List<ConsumerRecord<K, V>>> processor,
+            Acknowledgment ack){
+
+        boolean success;
+        try {
+            processor.proccess(records);
+            success = true;
+        }catch (Exception e){
+            reporter.reportProcessingError(records, e);
+            success = false;
+        }finally {
+            if(ack != null) { ack.acknowledge(); }
+        }
+        return success;
+    }
+
+
+    private void processAllErrorLoop(
+            List<ConsumerRecord<K, V>> records,
+            Processor<List<ConsumerRecord<K, V>>> processor,
+            Acknowledgment ack){
+
+        if(records == null) throw new IllegalArgumentException("records must not be null");
+        if(processor == null) throw new IllegalArgumentException("processor must not be null");
+        if(ack == null) throw new IllegalArgumentException("ack must not be null");
+
+        int errorLoopIteration = 0;
+        boolean errorLoop;
+
+        do{
+            try{
+                processor.proccess(records);
+                ack.acknowledge();
+                errorLoop = false;
+            }catch (Exception e){
+                errorLoop = true;
+                ++errorLoopIteration;
+                log.warn("Error while processing records! Retry " + errorLoopIteration, e);
+
+                reporter.reportProcessingError(records, e, errorLoopIteration);
+
+                try {
+                    // Error Backoff
+                    Thread.sleep(Math.min(errorLoopIteration * 1000, 1000*60));
+                } catch (InterruptedException e1) { }
+            }
+
+        } while (errorLoop);
+    }
+
+    private ConsumerRecord<K, V> convertRecord(ConsumerRecord<K, Json> record){
         if(record.value() != null){
             try{
-                V value = record.value().json(clazz);
-                return ConsumerRecordBuilder.fromRecordWithValue(record, null);
+                V value = record.value().json(valueClazz);
+                return ConsumerRecordBuilder.fromRecordWithValue(record, value);
             }catch (JsonParseException e){
                 // Not even valid json! Assume poison message.
-                reporter.reportPoisonRecordSkipped(record, e); // Report
+                reporter.reportMalformedRecord(record, e); // Report
                 return null; // Skip
             }catch (JsonMappingException e) {
-                // TODO Maybe add switch to support skipping json mapping errors aswell
-                throw e; // For now just rethrow
+
+                reporter.reportMalformedRecord(record, e);
+
+                if(skipOnDtoMappingError){
+                    return null; // Skip
+                }else{
+                    throw e; // Escalate
+                }
             }
         }else{
             return ConsumerRecordBuilder.fromRecordWithValue(record, null);
--- spring-boot-starter-kafka/src/main/java/com/elderbyte/kafka/metrics/MetricsReporter.java
+++ spring-boot-starter-kafka/src/main/java/com/elderbyte/kafka/metrics/MetricsReporter.java
@@ -1,10 +1,19 @@
 package com.elderbyte.kafka.metrics;
 
 import com.elderbyte.kafka.serialisation.Json;
-import com.elderbyte.kafka.serialisation.JsonParseException;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+import java.util.Collection;
 
 public interface MetricsReporter {
 
-    void reportPoisonRecordSkipped(ConsumerRecord<?, ?> record, JsonParseException e);
+    void reportStreamingMetrics(int recordCount, long durationMs);
+
+    void reportMalformedRecord(ConsumerRecord<?, ?> record, Exception e);
+
+    <K> void reportUnrecoverableCrash(Collection<ConsumerRecord<K, Json>> rawRecords, Exception e);
+
+    <K, V> void reportProcessingError(Collection<ConsumerRecord<K,V>> records, Exception e);
+
+    <K, V> void reportProcessingError(Collection<ConsumerRecord<K,V>> records, Exception e, int errorLoopIteration);
 }
