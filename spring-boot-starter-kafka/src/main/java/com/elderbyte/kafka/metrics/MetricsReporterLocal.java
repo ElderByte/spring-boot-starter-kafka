3c913a0080d9bed77f30ed8cc2b9869394085ed2
IsNull <p.buettiker@elderbyte.com>
2018-05-21 14:47:17 +0200

    local metrics reporter report to console

--- spring-boot-starter-kafka/src/main/java/com/elderbyte/kafka/metrics/MetricsReporterLocal.java
+++ spring-boot-starter-kafka/src/main/java/com/elderbyte/kafka/metrics/MetricsReporterLocal.java
@@ -12,29 +12,31 @@ public class MetricsReporterLocal implements MetricsReporter {
 
     private final Logger log = LoggerFactory.getLogger(ManagedJsonProcessor.class);
 
-
     @Override
     public void reportStreamingMetrics(MetricsContext context, int recordCount, long durationNano) {
-
+        if(log.isDebugEnabled()){
+            var durationMs = durationNano / (1000 * 1000);
+            log.debug(context.toString() + ": Batch of "+recordCount+" records processed in " + durationMs + "ms." );
+        }
     }
 
     @Override
     public void reportMalformedRecord(MetricsContext context, ConsumerRecord<?, ?> record, Exception e) {
-
+        log.warn(context.toString() + ": Failed to decode record: " + record.toString(), e);
     }
 
     @Override
     public <K> void reportUnrecoverableCrash(MetricsContext context, Collection<ConsumerRecord<K, Json>> rawRecords, Exception e) {
-
+        log.error(context.toString() + ": Failed hard for " + rawRecords.stream() + " records.", e);
     }
 
     @Override
     public <K, V> void reportProcessingError(MetricsContext context, Collection<ConsumerRecord<K, V>> consumerRecords, Exception e) {
-
+        log.warn(context.toString() + ": Failed to process records: " + consumerRecords.toString(), e);
     }
 
     @Override
     public <K, V> void reportProcessingError(MetricsContext context, Collection<ConsumerRecord<K, V>> consumerRecords, Exception e, int errorLoopIteration) {
-
+        log.warn(context.toString() + ": Failed to process records: " + consumerRecords.toString() + ". Error Loop Iteration: " + errorLoopIteration, e);
     }
 }
