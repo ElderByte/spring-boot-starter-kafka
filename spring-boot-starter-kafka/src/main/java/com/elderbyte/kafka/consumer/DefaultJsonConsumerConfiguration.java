6e9921c8ea29a7dfcaac56b556ac140d76854e8a
IsNull <p.buettiker@elderbyte.com>
2018-06-13 15:25:26 +0200

    added support for consumer factories

--- spring-boot-starter-kafka/src/main/java/com/elderbyte/kafka/consumer/DefaultJsonConsumerConfiguration.java
+++ spring-boot-starter-kafka/src/main/java/com/elderbyte/kafka/consumer/DefaultJsonConsumerConfiguration.java
@@ -4,13 +4,17 @@ import com.elderbyte.kafka.config.KafkaClientConfig;
 import com.elderbyte.kafka.serialisation.Json;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.AbstractMessageListenerContainer;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,8 +29,6 @@ public class DefaultJsonConsumerConfiguration {
      *                                                                         *
      **************************************************************************/
 
-    public static final String JSON_BATCH_FACTORY = "kafkaBatchFactory";
-
     @Autowired
     private KafkaClientConfig config;
 
@@ -35,7 +37,42 @@ public class DefaultJsonConsumerConfiguration {
      * Public API                                                              *
      *                                                                         *
      **************************************************************************/
+    
+    /**
+     * Default factory as string
+     */
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>>
+    kafkaListenerContainerFactory() {
+        var factory = buildStringContainerFactory(consumerFactoryStringString());
+        return factory;
+    }
 
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactoryStringString() {
+        var factory = new DefaultKafkaConsumerFactory<String, String>(consumerConfigs());
+        factory.setKeyDeserializer(new StringDeserializer());
+        factory.setValueDeserializer(new StringDeserializer());
+
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, byte[]> consumerFactoryStringByte() {
+        var factory = new DefaultKafkaConsumerFactory<String, byte[]>(consumerConfigs());
+        factory.setKeyDeserializer(new StringDeserializer());
+        factory.setValueDeserializer(new ByteArrayDeserializer());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<byte[], byte[]> consumerFactoryByteByte() {
+        var factory = new DefaultKafkaConsumerFactory<byte[], byte[]>(consumerConfigs());
+        factory.setKeyDeserializer(new ByteArrayDeserializer());
+        factory.setValueDeserializer(new ByteArrayDeserializer());
+        return factory;
+    }
 
     @Bean
     public Map<String, Object> consumerConfigs() {
@@ -53,8 +90,8 @@ public class DefaultJsonConsumerConfiguration {
      *                                                                         *
      **************************************************************************/
 
-    private ConcurrentKafkaListenerContainerFactory<String, Json> buildJsonContainerFactory(ConsumerFactory<String, Json> consumerFactory){
-        var factory = new ConcurrentKafkaListenerContainerFactory<String, Json>();
+    private ConcurrentKafkaListenerContainerFactory<String, String> buildStringContainerFactory(ConsumerFactory<String, String> consumerFactory){
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
         factory.setConsumerFactory(consumerFactory);
         config.getConsumerConcurrency().ifPresent(c -> factory.setConcurrency(c));
         config.getConsumerPollTimeout().ifPresent(t -> factory.getContainerProperties().setPollTimeout(t));
@@ -64,7 +101,6 @@ public class DefaultJsonConsumerConfiguration {
                         ? AbstractMessageListenerContainer.AckMode.BATCH
                         : AbstractMessageListenerContainer.AckMode.MANUAL
         );
-
 
         return factory;
     }
