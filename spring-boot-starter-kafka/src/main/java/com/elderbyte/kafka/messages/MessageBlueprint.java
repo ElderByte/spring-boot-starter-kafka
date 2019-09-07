136111d9883ccf02142793e4e87a4fcc35b6cdec
IsNull <p.buettiker@elderbyte.com>
2019-09-07 11:32:27 +0200

    support populate/write-metadata

--- spring-boot-starter-kafka/src/main/java/com/elderbyte/kafka/messages/MessageBlueprint.java
+++ spring-boot-starter-kafka/src/main/java/com/elderbyte/kafka/messages/MessageBlueprint.java
@@ -11,6 +11,13 @@ import static java.util.stream.Collectors.toMap;
 
 public class MessageBlueprint {
 
+
+    /***************************************************************************
+     *                                                                         *
+     * Fields                                                                  *
+     *                                                                         *
+     **************************************************************************/
+
     private final boolean tombstone;
     private final Field keyField;
 
@@ -18,6 +25,13 @@ public class MessageBlueprint {
      * Header-Key : Field
      */
     private final Map<String, MetadataField> metadataFields;
+
+
+    /***************************************************************************
+     *                                                                         *
+     * Constructor                                                             *
+     *                                                                         *
+     **************************************************************************/
 
     public MessageBlueprint(
             boolean tomstone,
@@ -35,9 +49,23 @@ public class MessageBlueprint {
     }
 
 
+    /***************************************************************************
+     *                                                                         *
+     * Properties                                                              *
+     *                                                                         *
+     **************************************************************************/
+
     public boolean isTombstone() {
         return tombstone;
     }
+
+
+    /***************************************************************************
+     *                                                                         *
+     * Public API                                                              *
+     *                                                                         *
+     **************************************************************************/
+
 
     public <V> String getKey(V message) {
         var value = getFieldAsString(keyField, message);
@@ -47,48 +75,57 @@ public class MessageBlueprint {
         return value;
     }
 
-    private <V, K> void setKey(V value, K key) {
-
-    }
-
     public <V> Map<String, String> getHeaders(V message) {
         var headers = new HashMap<String, String>();
         metadataFields.values().forEach(
                 f -> {
-
-                    var val = getFieldAsString(f.getField(), message);
-                    if(val != null){
-                        headers.put(
-                                f.getMetadataKey(),
-                                val
-                        );
+                    if(f.isWriteToMetadata()){
+                        var val = getFieldAsString(f.getField(), message);
+                        if(val != null){
+                            headers.put(
+                                    f.getMetadataKey(),
+                                    val
+                            );
+                        }
                     }
                 }
         );
         return headers;
     }
 
-    public   <V, K> V updateFromRecord(ConsumerRecord<K, V> record) {
+    public <V, K> V updateFromRecord(ConsumerRecord<K, V> record) {
         return updateFromRecord(record.value(), record);
     }
 
-    public  <V, K, M> M updateFromRecord(M message, ConsumerRecord<K, V> record) {
+    public <V, K, M> M updateFromRecord(M message, ConsumerRecord<K, V> record) {
 
-        if(getKey(message) == null){
-            setKey(message, record.key());
+        if(getKey(message) == null && record.key() != null){
+            setFieldString(keyField, message, record.key().toString());
         }
 
         var headers = record.headers();
 
         metadataFields.forEach((k,field) -> {
-            var header = headers.lastHeader(k);
-            if(header != null){
-                setField(field.getField(), message, header.value());
+            if(field.isPopulate()){
+
+                // Support Map<String,String> with all headers
+
+                var header = headers.lastHeader(k);
+                if(header != null){
+                    setField(field.getField(), message, header.value());
+                }
             }
         });
 
         return message;
     }
+
+
+    /***************************************************************************
+     *                                                                         *
+     * Private methods                                                         *
+     *                                                                         *
+     **************************************************************************/
 
     private <V> String getFieldAsString(Field field, V message){
         try {
@@ -111,16 +148,12 @@ public class MessageBlueprint {
             if(field.getType() == String.class){
                 field.set(message, headerValue);
             }else{
-                throw new InvalidMessageException("Only strings are supported as targets but field" +
-                        " "+field.getName()+" was of type "+field.getType()+" ! ");
+                throw new InvalidMessageException("Field " +
+                        " "+field.getName()+" was of unsupported type "+field.getType()+" ! ");
             }
         } catch (IllegalAccessException e) {
             throw new InvalidMessageException("Failed to access value of key field: "+keyField.getName(), e);
         }
     }
-
-
-
-
 
 }
--- spring-boot-starter-kafka/src/main/java/com/elderbyte/kafka/messages/MetadataField.java
+++ spring-boot-starter-kafka/src/main/java/com/elderbyte/kafka/messages/MetadataField.java
@@ -12,25 +12,34 @@ public class MetadataField {
     public static MetadataField from(Field field, @Nullable MessageMetadata metadataAttr){
 
         String metadataKey;
+        var writeToMetadata = true;
+        var populate = true;
 
         if(metadataAttr != null && StringUtils.hasText(metadataAttr.key())){
             metadataKey = metadataAttr.key();
+            writeToMetadata = metadataAttr.writeToMetadata();
+            populate = metadataAttr.populate();
         }else{
             metadataKey = field.getName();
         }
-        return new MetadataField(field, metadataKey);
+
+        return new MetadataField(field, metadataKey, writeToMetadata, populate);
     }
 
     private final Field field;
     private final String metadataKey;
+    private final boolean writeToMetadata;
+    private final boolean populate;
 
-    public MetadataField(Field field, String metadataKey) {
+    public MetadataField(Field field, String metadataKey, boolean writeToMetadata, boolean populate) {
 
         if(field == null) throw new ArgumentNullException("field");
         if(!StringUtils.hasText(metadataKey)) throw new IllegalArgumentException("metadataKey must have text but was: '"+metadataKey+"'");
 
         this.field = field;
         this.metadataKey = metadataKey;
+        this.writeToMetadata = writeToMetadata;
+        this.populate = populate;
     }
 
     public Field getField() {
@@ -39,5 +48,13 @@ public class MetadataField {
 
     public String getMetadataKey() {
         return metadataKey;
+    }
+
+    public boolean isWriteToMetadata() {
+        return writeToMetadata;
+    }
+
+    public boolean isPopulate() {
+        return populate;
     }
 }
