fb78aac6fa4da685c6c8bc22738674e5ca737d76
IsNull <p.buettiker@elderbyte.com>
2019-09-24 09:35:39 +0200

    make kafka-streams dependency optional

--- spring-boot-starter-kafka/src/main/java/com/elderbyte/kafka/streams/ElderKafkaStreamsConfiguration.java
+++ spring-boot-starter-kafka/src/main/java/com/elderbyte/kafka/streams/ElderKafkaStreamsConfiguration.java
@@ -4,12 +4,19 @@ import com.elderbyte.kafka.config.KafkaClientProperties;
 import com.elderbyte.kafka.streams.factory.KafkaStreamsContextBuilderFactory;
 import com.elderbyte.kafka.streams.factory.KafkaStreamsContextFactoryImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.streams.KafkaStreams;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
+/**
+ * Configure Kafka Streams Support
+ *
+ * (Only when Kafka Streams is on the class path)
+ */
 @Configuration
+@ConditionalOnClass(KafkaStreams.class)
 public class ElderKafkaStreamsConfiguration {
 
     @Autowired
