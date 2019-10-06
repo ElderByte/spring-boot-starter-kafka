9221d0d3d1c0942516585dcc31799d290a1f7c56
IsNull <p.buettiker@elderbyte.com>
2019-10-06 17:03:24 +0200

    improve default kafka-admin configuration (auto-commit off when no group-id is defined)

--- spring-boot-starter-kafka/src/main/java/com/elderbyte/kafka/admin/DefaultKafkaAdminConfiguration.java
+++ spring-boot-starter-kafka/src/main/java/com/elderbyte/kafka/admin/DefaultKafkaAdminConfiguration.java
@@ -1,8 +1,11 @@
 package com.elderbyte.kafka.admin;
 
 import com.elderbyte.kafka.config.KafkaClientProperties;
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +13,7 @@ import org.springframework.kafka.core.KafkaAdmin;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @Configuration
 @ConditionalOnProperty(value = "kafka.client.admin.enabled", matchIfMissing = true)
@@ -23,6 +27,9 @@ public class DefaultKafkaAdminConfiguration {
 
     @Autowired
     private KafkaClientProperties config;
+
+    @Value("${spring.application.name:}")
+    private String applicationName;
 
     /***************************************************************************
      *                                                                         *
@@ -49,7 +56,20 @@ public class DefaultKafkaAdminConfiguration {
     private Map<String, Object> adminConfig(){
         var configs = new HashMap<String, Object>();
         configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, config.getServers());
+        configs.put(AdminClientConfig.CLIENT_ID_CONFIG, defaultAdminClientId());
+        configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
         return configs;
+    }
+
+    private String defaultAdminClientId(){
+        String appName;
+        if(applicationName != null && !applicationName.isBlank()){
+            appName = applicationName;
+        }else{
+            var uuidParts = UUID.randomUUID().toString().split("-");
+            appName = "spring-" + uuidParts[uuidParts.length-1];
+        }
+        return appName + "-admin";
     }
 
 }
